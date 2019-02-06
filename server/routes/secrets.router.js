bd82eb8e404af20138db32e629e2744a9c5fe8dd
Matt Kleven <klevennb115@gmail.com>
2019-02-06 13:42:47 -0600

    finished assignment

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    console.log('req.user:', req.user.clearance_level);
+    if (req.isAuthenticated()) {
+       pool.query(` SELECT * FROM "secret"
+                    WHERE "secrecy_level" < ${req.user.clearance_level};`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
-        });
+        }); 
+    } else {
+        res.sendStatus(403);
+    }
+    
 });
 
 module.exports = router;
